735f41c69ebf586f8a76d40422418dc22780200f
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-18 00:19:13 +0100

    Input GetAxis example added

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        float verticalThrow = Input.GetAxis("Vertical");
+        Debug.Log(horizontalThrow);
+        Debug.Log(verticalThrow);
+    }
+}
