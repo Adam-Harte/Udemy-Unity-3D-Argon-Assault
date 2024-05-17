732d7ac27abab9a5b85e2616e774841837ebd02c
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-18 00:31:30 +0100

    InputSystem example added

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,18 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField]
+    InputAction movement;
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
+
+        // float horizontalThrow = Input.GetAxis("Horizontal");
+        // float verticalThrow = Input.GetAxis("Vertical");
+
         Debug.Log(horizontalThrow);
         Debug.Log(verticalThrow);
     }
