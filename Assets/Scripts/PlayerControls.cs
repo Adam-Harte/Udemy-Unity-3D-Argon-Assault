1ff156da47990ddceec767bbd04bee60eac7371d
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-18 16:28:36 +0100

    local rotation added

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
@@ -11,6 +12,17 @@ public class PlayerControls : MonoBehaviour
     float xRange = 10f;
     [SerializeField]
     float yRange = 7f;
+    [SerializeField]
+    float positionPitchFactor = -2f;
+    [SerializeField]
+    float controlPitchFactor = -15f;
+    [SerializeField]
+    float positionYawFactor = 2f;
+    [SerializeField]
+    float controlRollFactor = -15f;
+
+    float horizontalThrow;
+    float verticalThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -31,14 +43,33 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = verticalThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = horizontalThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        horizontalThrow = movement.ReadValue<Vector2>().x;
+        verticalThrow = movement.ReadValue<Vector2>().y;
 
         float xOffset = horizontalThrow * controlSpeed * Time.deltaTime;
         float yOffset = verticalThrow * controlSpeed * Time.deltaTime;
         float newXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(newXPos, -xRange, xRange);
-        float newYPos = transform.localPosition.x + yOffset;
+        float newYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(newYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
