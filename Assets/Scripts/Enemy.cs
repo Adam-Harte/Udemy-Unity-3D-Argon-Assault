9ba8be0985ce4fe8fb7ba875b7529437ede8188c
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-19 23:34:55 +0100

    ScoreBoard logic added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    GameObject deathVfx;
+
+    ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
     private void OnParticleCollision(GameObject other)
     {
+        Instantiate(deathVfx, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        scoreBoard.IncreaseScore(1);
     }
 }
