51e1ce5c6c762771118a59bf83b7ddc4ce6f1272
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-20 16:06:52 +0100

    ui score board example added

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    private void Start() {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "Start";
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
+        scoreText.text = score.ToString();
     }
 }
