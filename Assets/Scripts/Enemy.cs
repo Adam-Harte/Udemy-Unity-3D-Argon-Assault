f618cc507c1ebf0dc7ff4e966ce1e1552b317809
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-20 16:30:16 +0100

    enemy variety prefabs added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,17 +4,34 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     GameObject deathVfx;
+    [SerializeField]
+    int hitPoints = 1;
+    [SerializeField]
+    int hitScoreBonus = 1;
+    [SerializeField]
+    int killScoreBonus = 1;
 
     ScoreBoard scoreBoard;
 
     private void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
     private void OnParticleCollision(GameObject other)
     {
+        hitPoints--;
+        scoreBoard.IncreaseScore(hitScoreBonus);
+
+        if (hitPoints < 1) {
+            DestroyEnemy(); 
+        }
+    }
+
+    private void DestroyEnemy() {
         Instantiate(deathVfx, transform.position, Quaternion.identity);
         Destroy(gameObject);
-        scoreBoard.IncreaseScore(1);
+        scoreBoard.IncreaseScore(killScoreBonus);
     }
 }
